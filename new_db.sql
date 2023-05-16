4f3e50208f62382eb5af941f210b78fed2680b0f
HunkHazard <jkabir.bscs21seecs@seecs.edu.pk>
2023-05-16 11:21:02 +0500

    updated db

--- new_db.sql
+++ new_db.sql
@@ -38,7 +38,7 @@ CREATE TABLE `Admin` (
 
 LOCK TABLES `Admin` WRITE;
 /*!40000 ALTER TABLE `Admin` DISABLE KEYS */;
-INSERT INTO `Admin` VALUES (1,'Jawad','Kabir','jkabir.bscs21seecs@seecs.edu.pk','jawad'),(2,'Uneeb','Aftab','muneeb.bscs21seecs@seecs.edu.pk','uneeb'),(3,'Ahad','Sajjad','asajjad.bscs21seecs@seecs.edu.pk','ahad');
+INSERT INTO `Admin` VALUES (1,'Jawad','Kabir','jkabir.bscs21seecs@seecs.edu.pk','$2a$10$x1wWExD.JLhNmyQCWPTJ9e7XtuxoqIiMbh4L/pqA.S2P3i6ot5aMq'),(2,'Uneeb','Aftab','muneeb.bscs21seecs@seecs.edu.pk','uneeb'),(3,'Ahad','Sajjad','asajjad.bscs21seecs@seecs.edu.pk','ahad');
 /*!40000 ALTER TABLE `Admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `Donation` (
   KEY `ProjectID` (`ProjectID`),
   CONSTRAINT `Donation_ibfk_1` FOREIGN KEY (`DonorID`) REFERENCES `Donor` (`DonorID`),
   CONSTRAINT `Donation_ibfk_2` FOREIGN KEY (`ProjectID`) REFERENCES `Project` (`ProjectID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `Donation` (
 
 LOCK TABLES `Donation` WRITE;
 /*!40000 ALTER TABLE `Donation` DISABLE KEYS */;
-INSERT INTO `Donation` VALUES (1,1,1,100,'2022-05-09 10:30:00'),(2,2,2,50,'2022-05-09 11:15:00'),(3,3,1,200,'2022-05-09 12:00:00'),(4,1,3,75,'2022-05-09 13:30:00'),(5,4,2,25,'2022-05-09 14:45:00'),(6,6,3,150,'2022-05-10 09:00:00'),(7,9,2,100,'2022-05-10 10:30:00'),(8,6,2,500,'2023-05-10 10:00:00'),(9,6,3,1000,'2023-05-10 11:00:00');
+INSERT INTO `Donation` VALUES (1,1,1,100,'2022-05-09 10:30:00'),(2,2,2,50,'2022-05-09 11:15:00'),(3,3,1,200,'2022-05-09 12:00:00'),(4,1,3,75,'2022-05-09 13:30:00'),(5,4,2,25,'2022-05-09 14:45:00'),(6,6,3,150,'2022-05-10 09:00:00'),(7,9,2,100,'2022-05-10 10:30:00'),(8,6,2,500,'2023-05-10 10:00:00'),(9,6,3,1000,'2023-05-10 11:00:00'),(10,6,5,3000,'2023-05-15 18:30:00'),(11,6,4,12000,'2023-05-15 23:30:00'),(12,8,1,150,'2023-05-16 00:39:00'),(13,8,1,100,'2023-05-16 00:40:00');
 /*!40000 ALTER TABLE `Donation` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -109,7 +109,7 @@ CREATE TABLE `Donor` (
   `Province` varchar(50) NOT NULL,
   `Password` varchar(255) NOT NULL,
   PRIMARY KEY (`DonorID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +118,7 @@ CREATE TABLE `Donor` (
 
 LOCK TABLES `Donor` WRITE;
 /*!40000 ALTER TABLE `Donor` DISABLE KEYS */;
-INSERT INTO `Donor` VALUES (1,'Ali','Ahmed','ali.ahmed@example.com','0321-1234567','Karachi','Sindh',''),(2,'Fatima','Khan','fatima.khan@example.com','0311-9876543','Lahore','Punjab',''),(3,'Asad','Hussain','asad.hussain@example.com','0333-2468101','Islamabad','Islamabad Capital Territory',''),(4,'Saba','Malik','saba.malik@example.com','0322-4567890','Rawalpindi','Punjab',''),(5,'Bilal','Iqbal','bilal.iqbal@example.com','0300-1357902','Multan','Punjab',''),(6,'Jawad','Kabir','jkabir.bscs21seecs@seecs.edu.pk','0333-2720921','Rawalpindi','Punjab','$2a$10$dP1Tk4bqdkMKIfVnqVw98OzGUAwO20wuxZKGfpBojqcvj5UaUpRRm'),(7,'ahad','sajjad','asajjad.bscs21seecs@seecs.edu.pk','1234-545454','Rawalpindi','Punjab','$2a$10$AjMix3Dn8mT7waGF3YGLF.ZPFiK4./AtdL2gYjNUX4xf4lixzuKqe'),(8,'Uneeb','Aftab','muneeb21seecs@seecs.edu.pk','1234-545454','Islamabad','Punjab','$2a$10$RiAxmoRYvKF.IcJBJhmcnO0nnuyBgty/0k/Garby4NmElNYMZAqNW'),(9,'arslan','tariq','atariq.bscs21seecs@seecs.edu.pk','1234-56789-0','Rawalpindi','P','$2a$10$jJxNrKN9rC74iWfys1I0H./zLFfrpnqDbh3/xz5n0qVcMeUlG1UYm'),(10,'admin','web','admin@admin.com','1234-56789-0','Rawalpindi','Punjab','$2a$10$IPYq0FBJtuUqIx00OTz8s.OzjzBY7erbqIzLFeC8s.IWpEzm6.YWS');
+INSERT INTO `Donor` VALUES (1,'Ali','Ahmed','ali.ahmed@example.com','0321-1234567','Karachi','Sindh',''),(2,'Fatima','Khan','fatima.khan@example.com','0311-9876543','Lahore','Punjab',''),(3,'Asad','Hussain','asad.hussain@example.com','0333-2468101','Islamabad','Islamabad Capital Territory',''),(4,'Saba','Malik','saba.malik@example.com','0322-4567890','Rawalpindi','Punjab',''),(5,'Bilal','Iqbal','bilal.iqbal@example.com','0300-1357902','Multan','Punjab',''),(6,'Jawad','Kabir','jkabir.bscs21seecs@seecs.edu.pk','0333-2720921','Rawalpindi','Punjab','$2a$10$dP1Tk4bqdkMKIfVnqVw98OzGUAwO20wuxZKGfpBojqcvj5UaUpRRm'),(7,'ahad','sajjad','asajjad.bscs21seecs@seecs.edu.pk','1234-545454','Rawalpindi','Punjab','$2a$10$AjMix3Dn8mT7waGF3YGLF.ZPFiK4./AtdL2gYjNUX4xf4lixzuKqe'),(8,'Uneeb','Aftab','muneeb21seecs@seecs.edu.pk','1234-545454','Islamabad','Punjab','$2a$10$RiAxmoRYvKF.IcJBJhmcnO0nnuyBgty/0k/Garby4NmElNYMZAqNW'),(9,'arslan','tariq','atariq.bscs21seecs@seecs.edu.pk','1234-56789-0','Rawalpindi','P','$2a$10$jJxNrKN9rC74iWfys1I0H./zLFfrpnqDbh3/xz5n0qVcMeUlG1UYm'),(10,'admin','web','admin@admin.com','1234-56789-0','Rawalpindi','Punjab','$2a$10$IPYq0FBJtuUqIx00OTz8s.OzjzBY7erbqIzLFeC8s.IWpEzm6.YWS'),(11,'Ligma','Balls','ligma@ball.com','123456789','Deez','Candice','$2a$10$XTQvqIeshRv8xEZhhi6iYeDl8sK/kX9FfL9eRxxjOuldZ.8EbvCFW'),(12,'','','','','','','$2a$10$bhskgydRRr6YHquEnrROges3i4c4m7RswAbAURYqYNOuWFUPhxkwa');
 /*!40000 ALTER TABLE `Donor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +175,7 @@ CREATE TABLE `Project` (
 
 LOCK TABLES `Project` WRITE;
 /*!40000 ALTER TABLE `Project` DISABLE KEYS */;
-INSERT INTO `Project` VALUES (1,'Food Distribution','Providing food to needy families during the pandemic',1,10000,300),(2,'Education for Orphans','Providing education to orphans in the village',2,5000,675),(3,'Cancer Treatment','Providing cancer treatment to patients who cannot afford it',3,7500,1225),(4,'Medical Camp','Providing medical treatment to people in remote areas',4,12000,0),(5,'Eye Surgery','Performing eye surgeries for patients with cataracts',5,9000,0);
+INSERT INTO `Project` VALUES (1,'Food Distribution','Providing food to needy families during the pandemic',1,10000,550),(2,'Education for Orphans','Providing education to orphans in the village',2,5000,675),(3,'Cancer Treatment','Providing cancer treatment to patients who cannot afford it',3,7500,1225),(4,'Medical Camp','Providing medical treatment to people in remote areas',4,12000,12000),(5,'Eye Surgery','Performing eye surgeries for patients with cataracts',5,9000,3000);
 /*!40000 ALTER TABLE `Project` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -188,4 +188,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-15 17:50:59
+-- Dump completed on 2023-05-16 11:20:50
